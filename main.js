e2cccf11a9a531e916a39864e8095a5fbbca5e8f
ShinseiKei <kei@shinseiind.co.jp>
2024-12-23 13:46:27 +0900

    version 1.0.0

--- main.js
+++ main.js
@@ -1,44 +1,73 @@
-/*global define, brackets, $ */
-
-// See detailed docs in https://docs.phcode.dev/api/creating-extensions
-
-// A good place to look for code examples for extensions: https://github.com/phcode-dev/phoenix/tree/main/src/extensions/default
-
-// A simple extension that adds an entry in "file menu> hello world"
 define(function (require, exports, module) {
     "use strict";
 
-    // Brackets modules
-    const AppInit = brackets.getModule("utils/AppInit"),
-        DefaultDialogs = brackets.getModule("widgets/DefaultDialogs"),
-        Dialogs = brackets.getModule("widgets/Dialogs"),
-        CommandManager = brackets.getModule("command/CommandManager"),
-        Menus = brackets.getModule("command/Menus");
+    var CommandManager = brackets.getModule("command/CommandManager");
+    var Menus = brackets.getModule("command/Menus");
+    var EditorManager = brackets.getModule("editor/EditorManager");
+    var PanelManager = brackets.getModule("view/PanelManager");
+    var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
 
-    // Function to run when the menu item is clicked
-    function handleHelloWorld() {
-        Dialogs.showModalDialog(
-            DefaultDialogs.DIALOG_ID_INFO,
-            "hello",
-            "world"
-        );
+    var panel;
+    var isPanelVisible = false;
+
+    // action
+    var actions = {
+        action1: function () { console.log("DocMaker Index作成"); },
+        action2: function () { console.log("DocMaker Page作成"); }
+    };
+
+    function handleTogglePanel() {
+        if (isPanelVisible) {
+            panel.hide();
+        } else {
+            if (!panel) {
+                // import html panel
+                ExtensionUtils.loadFile(module, "panel.html").done(function(panelHTML) {
+                    panel = PanelManager.createBottomPanel("DocMaker.panel", $(panelHTML), 100);
+
+                    // click 
+                    $("#DocMaker-panel").on("click", ".custom-button", function () {
+                        var type = $(this).data("type");
+                        if (type === "template") {
+                            // insert template
+                            var filePath = $(this).data("file");
+                            ExtensionUtils.loadFile(module, filePath).done(function(content) {
+                                var editor = EditorManager.getActiveEditor();
+                                if (editor) {
+                                    var codeMirror = editor._codeMirror;
+                                    var doc = codeMirror.getDoc();
+                                    var cursor = doc.getCursor();
+                                    doc.replaceRange(content, cursor);
+                                }
+                            }).fail(function() {
+                                console.error("file import error：" + filePath);
+                            });
+                        } else if (type === "action") {
+                            // action process
+                            var actionName = $(this).data("action");
+                            var action = actions[actionName];
+                            if (action) {
+                                action();
+                            } else {
+                                console.error("undefine action：" + actionName);
+                            }
+                        }
+                    });
+
+                    panel.show();
+                }).fail(function() {
+                    console.error("can't import panel file");
+                });
+            } else {
+                panel.show();
+            }
+        }
+        isPanelVisible = !isPanelVisible;
     }
-    
-      // First, register a command - a UI-less object associating an id to a handler
-    var MY_COMMAND_ID = "helloworld.sayhello";   // package-style naming to avoid collisions
-    CommandManager.register("Hello World", MY_COMMAND_ID, handleHelloWorld);
 
-    // Then create a menu item bound to the command
-    // The label of the menu item is the name we gave the command (see above)
-    var menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
-    menu.addMenuItem(MY_COMMAND_ID);
-    
-    // We could also add a key binding at the same time:
-    //menu.addMenuItem(MY_COMMAND_ID, "Ctrl-Alt-W");
-    // (Note: "Ctrl" is automatically mapped to "Cmd" on Mac)
-    
-    // Initialize extension once shell is finished initializing.
-    AppInit.appReady(function () {
-        console.log("hello world");
-    });
+    var DOCMAKER_COMMAND_ID = "DocMaker.togglePanel";
+    CommandManager.register("DocMaker", DOCMAKER_COMMAND_ID, handleTogglePanel);
+
+    var menu = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU);
+    menu.addMenuItem(DOCMAKER_COMMAND_ID);
 });
